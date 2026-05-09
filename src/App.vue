e531e1a4d18a51de91e81a0b9ef79c4d12edaadf
zhongtan <zhongtan238@gmail.com>
2026-05-09 19:23:51 +0800

    04-scss文件自动导入

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <!-- 一级路由出口组件 -->
   <RouterView></RouterView>
+  <div class="test">test scss</div>
 </template>
 
 <script setup lang="ts" name="App">
 
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+  .test {
+    color: $xtxColor;
+  }
 </style>
