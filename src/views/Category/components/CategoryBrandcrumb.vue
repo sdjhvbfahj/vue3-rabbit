03651289fda12db27bf725da304c5eba4e4605c3
zhongtan <zhongtan238@gmail.com>
2026-05-12 00:49:54 +0800

    20-Category-面包屑导航组件封装

---
+++ src/views/Category/components/CategoryBrandcrumb.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="CategoryBrandcrumb">
+        <el-breadcrumb :separator-icon="ArrowRight">
+            <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item>{{ category }}</el-breadcrumb-item>
+        </el-breadcrumb>
+    </div>
+</template>
+
+<script setup lang="ts" name="CategoryBrandcrumb">
+    import {ArrowRight} from '@element-plus/icons-vue'
+    defineProps(['category']);
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,19 +1,16 @@
 <template>
     <div class="Category wrapper">
-        <div class="brandcrumb">
-            <el-breadcrumb :separator-icon="ArrowRight">
-                <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-                <el-breadcrumb-item>{{ category.name }}</el-breadcrumb-item>
-            </el-breadcrumb>
-        </div>
+        <CategoryBrandcrumb :category="category.name"/>
     </div>
 </template>
 
 <script setup lang="ts" name="Category">
-    import {ArrowRight} from '@element-plus/icons-vue'
     import {getCategoryAPI, type CategoryItem} from '@/apis/category.ts'
     import {useRoute} from 'vue-router'
     import {ref, onMounted, onBeforeUpdate} from 'vue'
+
+    import CategoryBrandcrumb from './components/CategoryBrandcrumb.vue'
+
     let category = ref<CategoryItem>({} as CategoryItem);
     // 拿到当前页面的路由信息
     const route = useRoute();
@@ -24,10 +21,10 @@
     }
     onMounted(() => {
         getCategory(route.params.id as string);
-    })
+    });
     onBeforeUpdate(() => {
         getCategory(route.params.id as string);
-    })
+    });
 </script>
 
 <style scoped>
